7bccad6f5c5c8a39c447d5d563461a51e3eed2b4
miegli <michae.egli@phlu.ch>
2017-11-18 10:38:12 +0100

    refactored

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var files = require('./lib/files');
 var replace = require('replace-in-file');
 var sha1 = require('sha1');
 var admin = require("firebase-admin");
-
+var encryption = require("./actions/encrypt");
+var build = require("./actions/build");
+var Observable = require('rxjs/Observable').Observable;
 
 clear();
 console.log(
@@ -19,91 +21,62 @@ console.log(
     )
 );
 
-
 if (!files.fileExists(files.getCurrentDirectory() + "/serviceAccountKey.json")) {
-    console.log(chalk.red('Error: ') +chalk('/serviceAccountKey.json not found. Please add firebase credential file in ')+chalk.yellow(files.getCurrentDirectory()));
+    console.log(chalk.red('Error: ') + chalk('/serviceAccountKey.json not found. Please add firebase credential file in ') + chalk.yellow(files.getCurrentDirectory()));
     process.exit(1);
 }
 
-
 var serviceAccount = require(files.getCurrentDirectory() + "/serviceAccountKey.json");
-
-console.log();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://"+serviceAccount.project_id+".firebaseio.com"
+    databaseURL: "https://" + serviceAccount.project_id + ".firebaseio.com"
 });
 
 
+var execute = function () {
 
+    return new Observable(function (observer) {
 
-var executeAppsAppEncryption = function (files) {
-
-
-    var replacements = {};
-
-    const options = {
-        files: files,
-        from: /\{\{\{[^}]+?\}\}\}/mg,
-        to: (data) => {
-
-            var hash = sha1(data);
-
-            replacements[hash] = data.substr(3,data.length-6);
-
-            return '{sha1://' + hash + '}';
-
-        },
-    };
-
-    try {
-        const changes = replace.sync(options);
-    }
-    catch (error) {
-        console.error('Error occurred:', error);
-    }
-
-
-    return replacements;
-
-
-};
-
-
-
-var replaceBuildFiles = function (src, callback) {
-    glob(src + '/www/build/main.js', callback);
-};
-replaceBuildFiles(files.getCurrentDirectory(), function (err, res) {
-
-    var status = new Spinner('Perform sha1 hash encryption for build files, please wait...');
-    status.start();
-
-    if (err || res.length == 0) {
-        console.log('No build files found. Enter something linke "npm run" or "ionic serve" first.');
-    } else {
-        let replacements = executeAppsAppEncryption(res);
-
-        if (Object.keys(replacements).length) {
-            var db = admin.database();
-            var ref = db.ref('_sha1');
-            ref.set(replacements).then(() => {
-                status.stop();
-                console.log(chalk.green('Build encryption sucessfully done.'));
-                process.exit(1);
-            });
-        } else {
-            setTimeout(function() {
-                status.stop();
-                console.log(chalk.green('Sucessfully done. ')+ chalk('Nothing to do. There are no more replacements for encryption found. Add any {{{your-secure-string}}} to your *.ts files.'));
-                status.stop();
-               // process.exit(1);
-            }, 2000);
+        let jobs = {
+            encryption: encryption,
+            build: build
         }
 
-    }
+        let successCount = 0;
+
+
+        Object.keys(jobs).forEach((job) => {
+            "use strict";
+
+            jobs[job]().then((next) => {
+                successCount++;
+                observer.next(next);
+                if (Object.keys(jobs).length == successCount) {
+                    observer.complete();
+                }
+            }).catch((error) => {
+                console.log(error);
+                process.exit();
+            });
+
+        });
+
+
+    });
+
+};
+
+
+execute().subscribe((next) => {
+   console.log(next);
+}, (err) => {
+    console.log(chalk.green(err));
+}, (done) => {
+    console.log(chalk.green('Done. '));
+    process.exit();
 });
 
 
-
+// set firebase config
+// firebase functions:config:set slack.url=https://hooks.slack.com/services/XXX
