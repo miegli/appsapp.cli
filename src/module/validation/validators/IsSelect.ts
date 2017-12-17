f412fce6391202f8d1d468a3d423ddfa620f53a7
miegli <michae.egli@phlu.ch>
2017-12-17 15:17:54 +0100

    added backend validator for select

--- src/module/validation/validators/IsSelect.ts
+++ src/module/validation/validators/IsSelect.ts
@@ -1,11 +1,15 @@
 import {registerDecorator, ValidationOptions, ValidationArguments} from "class-validator";
+import {Observer} from "rxjs/Observer";
+import {Observable} from "rxjs/Observable";
+import * as Unirest from "unirest";
 
 export function IsSelect(options?: {
     source?: {
         url: string,
         mapping: {
-            text: string,
-            value: string,
+            text: string | Function,
+            value: string | Function,
+            disabled?: boolean | Function,
             group?: string
         },
         type?: 'json' | 'jsonp'
@@ -26,7 +30,77 @@ export function IsSelect(options?: {
             constraints: [{'type': 'isSelect', value: options}],
             validator: {
                 validate(value: any, args: ValidationArguments) {
-                    return true;
+                    
+                    return new Promise(function (resolve, reject) {
+
+                        let optionValidator = {
+                            target: value,
+                            source: args.constraints[0].value.source,
+                            getOptions: () => {
+                                return new Promise(function (resolve, reject) {
+                                    if (optionValidator.source) {
+                                        Unirest.get(optionValidator.source.url).type('json').end(function (response) {
+                                            let options = [];
+                                            if (response.error) {
+                                                reject(response.error);
+                                            } else {
+                                                response.body.forEach((item) => {
+                                                    options.push({
+                                                        value: optionValidator._getPropertyFromObject(item, optionValidator.source.mapping.value),
+                                                        text: optionValidator._getPropertyFromObject(item, optionValidator.source.mapping.text),
+                                                        disabled: optionValidator.source.mapping.disabled !== undefined ? optionValidator._getPropertyFromObject(item, optionValidator.source.mapping.disabled) : false,
+                                                    })
+                                                });
+
+                                                resolve(options);
+                                            }
+                                        });
+                                    } else {
+                                        resolve(args.constraints[0].value.options);
+                                    }
+                                });
+                            },
+
+                            _getPropertyFromObject: (inputObject, property) => {
+
+                                if (typeof property == 'function') {
+                                    return inputObject !== undefined ? property(inputObject) : null;
+                                }
+
+                                if (property.indexOf(".") > 0) {
+                                    return optionValidator._getPropertyFromObject(inputObject[property.substr(0, property.indexOf("."))], property.substr(property.indexOf(".") + 1));
+                                } else {
+                                    return inputObject[property];
+                                }
+
+                            }
+                        }
+
+                        optionValidator.getOptions().then((options: any) => {
+
+                            let allValide = true;
+                            let values = {};
+                            options.forEach((option) => {
+                                if (!option.disabled) {
+                                    values[option.value] = true;
+                                }
+                            });
+                            optionValidator.target.forEach((value) => {
+                                if (values[value] == undefined) {
+                                    allValide = false;
+                                }
+                            });
+
+                            resolve(allValide);
+
+                        }).catch((error) => {
+                            resolve(false);
+                        });
+
+
+                    });
+
+
                 }
             }
         });
