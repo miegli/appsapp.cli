501ff7e95fd2d06706ec76f3363c6c60c973949f
miegli <michae.egli@phlu.ch>
2018-02-23 19:06:26 +0100

    bug fix

--- src/module/models/persistable.js
+++ src/module/models/persistable.js
@@ -1015,7 +1015,7 @@ var PersistableModel = /** @class */ (function () {
             return typeof value == 'object' ? value : [];
         }
         if (this.getMetadata(property, 'isList').length) {
-            var valueAsObjects_1 = this.createListArray(property);
+            var valueAsObjects_1 = this.createListArray(property, true);
             if (value && typeof value.forEach !== 'function') {
                 var tmp = [];
                 Object.keys(value).forEach(function (v) {
@@ -1028,7 +1028,7 @@ var PersistableModel = /** @class */ (function () {
                     if (itemOriginal !== undefined && itemOriginal && itemOriginal instanceof PersistableModel == false) {
                         var uuid = itemOriginal[self.getMetadataValue(property, 'isList', null, 'usePropertyAsUuid')];
                         var item_1 = null;
-                        if (self.getAppsAppModuleProvider() !== undefined) {
+                        if (!self.isInBackendMode()) {
                             item_1 = self.getAppsAppModuleProvider().new(self.getMetadataValue(property, 'isList'), uuid);
                         }
                         else {
@@ -1645,8 +1645,16 @@ var PersistableModel = /** @class */ (function () {
      * @param property
      * @returns {any}
      */
-    PersistableModel.prototype.createListArray = function (property) {
+    PersistableModel.prototype.createListArray = function (property, reset) {
         var self = this;
+        if (reset !== undefined) {
+            try {
+                delete this.__listArrays[property];
+            }
+            catch (e) {
+                //
+            }
+        }
         if (!self.isInBackendMode() && this.__listArrays[property] === undefined) {
             this.__listArrays[property] = new Array();
             self.watch(property, function (value) {
@@ -1670,7 +1678,7 @@ var PersistableModel = /** @class */ (function () {
         var properties = {}, v = value == undefined ? this.getPropertyValue(property) : value;
         if (v && v.length) {
             v.forEach(function (item) {
-                if (item && item instanceof PersistableModel) {
+                if (item && item instanceof PersistableModel && item.getUuid().length) {
                     properties[item.getUuid()] = {
                         value: item,
                         enumerable: false,
