dcfcb7a2d605d2839b415d1a224a2cda918f5e3a
miegli <michae.egli@phlu.ch>
2018-02-23 17:59:05 +0100

    1.6.192

--- dist/index.js
+++ dist/index.js
@@ -178,7 +178,6 @@ var PersistableModel = /** @class */ (function () {
         if (this.__observer) {
             this.__observer.next(this);
         }
-        this.convertListPropertiesFromArrayToObject();
         return this;
     };
     /**
@@ -798,7 +797,7 @@ var PersistableModel = /** @class */ (function () {
         Object.keys(self).forEach(function (property) {
             if (property.substr(0, 1) !== '_' && self.getMetadataValue(property, 'isList', null, 'usePropertyAsUuid')) {
                 var /** @type {?} */ tmp_1 = {}, /** @type {?} */ usePropertyAsUuid_1 = self.getMetadataValue(property, 'isList', null, 'usePropertyAsUuid');
-                if (usePropertyAsUuid_1 && usePropertyAsUuid_1 !== undefined && usePropertyAsUuid_1 !== true) {
+                if (usePropertyAsUuid_1 && usePropertyAsUuid_1 !== undefined && usePropertyAsUuid_1 !== true && self.getPropertyValue(property) && self.getPropertyValue(property).length) {
                     self.getPropertyValue(property).forEach(function (val) {
                         if (val[usePropertyAsUuid_1] !== undefined) {
                             tmp_1[val[usePropertyAsUuid_1]] = val;
